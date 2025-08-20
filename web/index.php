58f411b639aa26eb51c44c4de830fbf7a72ccdf3
kkuhn <kkuhn@kkuhn.42.fr>
2025-08-20 16:48:28 +0100

    database setup finish

--- web/index.php
+++ web/index.php
@@ -4,15 +4,24 @@
 </script>
 <h2>
     <?php
+	ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Ausgabe zum Testen
+	
 	$host = 'db'; // MariaDB-Container-Name oder Host (im Docker-Setup ist 'db' der Name des MariaDB-Containers)
 	$dbname = 'wordpress'; // Name der Datenbank
 	$username = 'wpuser'; // Dein MariaDB-Benutzername
 	$password = 'password'; // Dein MariaDB-Passwo
 
 try {
+echo "Versuche Verbindung zur Datenbank...<br>";
+
     // PDO-Verbindung zur MariaDB-Datenbank
     $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Fehlerbehandlung aktivieren
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "Verbindung zur Datenbank erfolgreich...<br>"; // Fehlerbehandlung aktivieren
 } catch (PDOException $e) {
     // Fehlerausgabe, falls die Verbindung fehlschlägt
     echo "Verbindung fehlgeschlagen: " . $e->getMessage();
@@ -20,7 +29,8 @@ try {
 }
     if(isset($_REQUEST['id']))
     {
-        $res = $db->query("SELECT name FROM Users WHERE id = ". $_REQUEST['id'])->fetch(PDO::FETCH_ASSOC); 
+echo "Request...<br>";
+        $res = $db->query("SELECT name FROM users WHERE id = ". $_REQUEST['id'])->fetch(PDO::FETCH_ASSOC); 
         echo "Willkommen". $res['name'] ." <a href='/delete'>(Account löschen)</a>";
     
     }
